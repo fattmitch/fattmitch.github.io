daba88e1c7cf801094747ac006b73671ffa8b3fd
fattmitch <matt.s.fitch@gmail.com>
2016-04-22 16:29:48 -0700

    about redo

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,40 +1,41 @@
 $(document).ready(function(){
     
-   $('#fullpage').fullpage({
-        anchor: ['top', 'work', ,'know-your-beer', 'venue-template', 'pay-crave', 'lil-bubbas', 'about', 'contact'],
-        menu: '#menu',
-        verticalCentered: false,
-        recordHistory: false,
-        navigation: true,
-        navigationPosition: 'right',
-        slidesNavigation: true,
-        slidesNavPosition: 'bottom',
-        afterLoad: function(anchorLink, index){
+  $('#fullpage').fullpage({
+    anchor: ['top', 'work', ,'know-your-beer', 'venue-template', 'pay-crave', 'lil-bubbas', 'about', 'contact'],
+    menu: '#menu',
+    verticalCentered: false,
+    recordHistory: false,
+    navigation: true,
+    navigationPosition: 'right',
+    slidesNavigation: true,
+    slidesNavPosition: 'bottom',
+    afterLoad: function(anchorLink, index){
             
-            if ($('.menu').hasClass('show')) {
-                toggleMenu();
-            }
-            if($('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').hasClass('open')) {
-                toggleIcon();
-            }
-        }
-    });
-    
-	$('#nav-icon3').click(function(){
-		toggleIcon();
-	});     
-    
-    $('.menu-button').click(function(event) {
+      if ($('.menu').hasClass('show')) {
         toggleMenu();
-    });
-    
-    function toggleMenu() {
-        $('.menu').toggleClass('show');
+      }
+      
+      if($('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').hasClass('open')) {
+        toggleIcon();
+      }
     }
+  });
     
-    function toggleIcon() {
-        $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').toggleClass('open');
-    }
+  $('#nav-icon3').click(function(){
+    toggleIcon();
+  });     
+
+  $('.menu-button').click(function(event) {
+    toggleMenu();
+  });
+
+  function toggleMenu() {
+    $('.menu').toggleClass('show');
+  }
+
+  function toggleIcon() {
+    $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').toggleClass('open');
+  }
   
   
 });
