2a50c915ada218f9136a8d6dd74c154cc192047b
fattmitch <matt.s.fitch@gmail.com>
2017-04-16 22:41:48 -0700

    removed fullPage.js functionality and references to prepare for redesign

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-    
-  $('#fullpage').fullpage({
+
+/*$('#fullpage').fullpage({
     anchor: ['top', 'work', ,'know-your-beer', 'venue-template', 'pay-crave', 'lil-bubbas', 'about', 'contact'],
     menu: '#menu',
     verticalCentered: false,
@@ -10,20 +10,20 @@ $(document).ready(function(){
     slidesNavigation: true,
     slidesNavPosition: 'bottom',
     afterLoad: function(anchorLink, index){
-            
+
       if ($('.menu').hasClass('show')) {
         toggleMenu();
       }
-      
+
       if($('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').hasClass('open')) {
         toggleIcon();
       }
     }
-  });
-    
+  });*/
+
   $('#nav-icon3').click(function(){
     toggleIcon();
-  });     
+  });
 
   $('.menu-button').click(function(event) {
     toggleMenu();
@@ -36,6 +36,5 @@ $(document).ready(function(){
   function toggleIcon() {
     $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').toggleClass('open');
   }
-  
   
 });
